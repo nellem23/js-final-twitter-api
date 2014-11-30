eac28aa89fa7a60892fef5a375b36aa4b22c2573
Nelle McDade <nelle@Nelles-MacBook-Pro.local>
2014-11-29 19:47:11 -0500

    forked

---
+++ config-sample.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'pmCBn9A28BcDLQzBnnQSXJXwC');
+define('CONSUMER_SECRET', 'cAgtK0yZw0HTh9lf3kXvJUtRWQdWYgMsmzXar5c4OubsT92TEj');
+define('OAUTH_CALLBACK', 'https://nellemcdade.com/twitteroauth/callback.php');
